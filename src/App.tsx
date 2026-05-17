22c07a588ccf702e95a40f578464be62241b2eb0
AdrianGF3 <adrian.gofa@gmail.com>
2026-05-17 09:28:34 +0200

    Inicio del proyecto, portafolio estará desarrollado con React+TS y tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="h-screen bg-black flex items-center justify-center">
+        <h1 className="text-white text-6xl font-bold">Portafolio</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
